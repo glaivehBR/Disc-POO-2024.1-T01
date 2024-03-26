1ef1facd451babcb7e458e0371800ed147e54ab6
Glaive Helles <125269667+glaivehBR@users.noreply.github.com>
2024-03-26 03:33:08 -0300

    Update principal.java

--- Cap 03/Exercicios-Resolvidos/QR-12/src/br/edu/principal/principal.java
+++ Cap 03/Exercicios-Resolvidos/QR-12/src/br/edu/principal/principal.java
@@ -2,7 +2,7 @@ package br.edu.principal;
 
 import java.util.Scanner;
 
-public class Principal {
+public class principal {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
